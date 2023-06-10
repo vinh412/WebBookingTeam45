61e0db0ea5c4e679e2b38f9ae62078c8cbcd65a6
hvan128 <ngohaivan7@gmail.com>
2023-06-10 21:50:00 +0700

    ContactDetail Component

--- client/src/pages/payment/Payment.jsx
+++ client/src/pages/payment/Payment.jsx
@@ -1,10 +1,12 @@
 import PaymentMethod from "../../components/payment/PaymentMethod";
+import InforDetail from "../../components/payment/InforDetail";
 
 const PayMent = () =>{
     return (
         <div>
             <h1>Payment Page</h1>
             <PaymentMethod/>
+            <InforDetail/>
         </div>
     )
 }
