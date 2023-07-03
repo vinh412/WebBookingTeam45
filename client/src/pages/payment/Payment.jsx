babca4c8ff104e4d0d3622288a40bc630c849292
hvan128 <ngohaivan7@gmail.com>
2023-07-03 22:51:07 +0700

    sidebar thanh toan

--- client/src/pages/payment/Payment.jsx
+++ client/src/pages/payment/Payment.jsx
@@ -1,17 +1,22 @@
 import PaymentMethod from "../../components/payment/PaymentMethod";
 import InforDetail from "../../components/payment/InforDetail";
-import NavBar from '../../components/navbar/NavBar'
+import NavBar from "../../components/navbar/NavBar";
+import Sidebar from "../../components/payment/Sidebar";
+import "./payment.css";
 
-
-
-const PayMent = () =>{
-    return (
+const PayMent = () => {
+  return (
+    <div className="payment-page">
+      <NavBar />
+      <div className="payment-page-content">
         <div>
-            <NavBar />
-            <PaymentMethod/>
-            <InforDetail/>
+          <PaymentMethod />
+          <InforDetail />
         </div>
-    )
-}
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
 
 export default PayMent;
